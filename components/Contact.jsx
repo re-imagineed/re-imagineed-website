a6ac0d53c7da0dce7dd20ca2e7b501609d7d5251
Quark <quark101@gmx.com>
2022-07-25 15:18:53 -0600

    Contact : Add divider

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components'
 import { device } from '../utils/media-breakpoints'
-
+import { StyledP2, StyledDividerLine } from './Styled'
 
 const StyledContact = styled.div`
     display: flex;
@@ -25,7 +25,7 @@ const StyledContactList = styled.span`
 const ContactItem = styled.div`
     margin-right: 60px;
     margin-bottom: 20px;
-
+    
     a {
         text-decoration: underline;
 
@@ -48,7 +48,9 @@ const ItemTitle = styled.span`
 function Contact(props) {
     return (
         <StyledContact>
-            <h2>Contact</h2>
+            <h1>Contact</h1>
+            <StyledDividerLine />
+            <div style={{ width: "100%", height: "20px" }}></div>
             <StyledContactList>
                 <ContactItem>
                     <p><ItemTitle name='true'>Isiah King</ItemTitle>
@@ -66,6 +68,7 @@ function Contact(props) {
                 </ContactItem>
                 <ContactItem><p>(414) 485-5817</p></ContactItem>
             </StyledContactList>
+        
         </StyledContact>
     )
 }
