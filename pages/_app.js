d9f8b6bdde1169ad7fe83fc058c5a123e7a3cc85
Quark <quark101@gmx.com>
2022-07-26 13:12:52 -0600

    Add colors from https://www.canva.com/learn/100-color-combinations/

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,42 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const theme = {
+  // #8 Primary Colors With a Vibrant Twist of https://www.canva.com/learn/100-color-combinations/
+  sky: '#375e97',
+  sunset: '#fb6542',
+  sunflower8: '#ffbb00',
+  grass: '#3f681c',
+
+  // #12 Surf & Turf of https://www.canva.com/learn/100-color-combinations/
+  sandstone: '#f4cc70',
+  burnt_orange: '#de7a22',
+  sea: '#20948b',
+  lagoon: '#6ab187',
+
+  // #23 Surf & Turf of https://www.canva.com/learn/100-color-combinations/
+  blue: '#4d85bd',
+  leafy_green: '#7caa2d',
+  sunflower23: '#f5e356',
+  seeds: '#cb6318',
+
+  // #28 Garden Fresh of https://www.canva.com/learn/100-color-combinations/
+  carrot: '#ee693f',
+  tangerine: '#f69454',
+  off_white28: '#fcfdfe',
+  pear_garden: '#739f3d',
+
+  // #37 Sunny Citrus of https://www.canva.com/learn/100-color-combinations/
+  citrus: '#faaf08',
+  grapefruit: '#fa812f',
+  ruby_red: '#fa4032',
+  off_white37: '#fef3e2',
+
+  // #44 Wintery Reds of https://www.canva.com/learn/100-color-combinations/
+  berry: '#a10115',
+  cherry: '#d72c16',
+  chiffon: '#f0efea',
+  smoke: '#c0b2b5',
+
   navy: '#012E55',
   lightgrey: '#EDEDED',
   grey: '#CCCCCC',
