40dde3c19b564abb22d267dac51e6dce3ae174f3
re-imagineed <101021811+re-imagineed@users.noreply.github.com>
2024-03-27 14:34:00 -0500

    Update team.js

--- pages/team.js
+++ pages/team.js
@@ -6,7 +6,7 @@ import BoardOfDirectors from '../components/BOD'
 const teamMembers = [
   {
     name: "Isiah King",
-    title: "Co-Founder & Co-Chief Executive Officer",
+    title: "Co-Founder & Managing Partner",
     imageURL: "/images/isiah_headshot.png",
     imageDimensions: { width: 280, height: 272},
     copy: [
@@ -18,7 +18,7 @@ const teamMembers = [
     name: "Keith Streicher",
     imageURL: "/images/keith-portrait2.jpeg",
     imageDimensions: { width: 340, height: 272},
-    title: "Co-Founder & Co-Chief Executive Officer",
+    title: "Co-Founder & Managing Principal",
     copy: [
       "Keith Streicher is one of the Co-Founders of Re-Imagine Education, Inc. He is a veteran of the U.S. Army and served his country in the mountains of the Republic of South Korea and in the valleys of Iraq Anbar province. Following his service, Mr. Streicher completed his bachelor’s degree at the Catholic University of America and an Executive Entrepreneurship Certificate Program from the Stanford Graduate School of Business. He has served Congresswoman Eleanor Holmes-Norton as an intern, Congressman Beto O'Rourke as a Military Legislative Assistant, and the Obama administration as a White House intern and United States Department of Veterans Affairs Secretarial Appointee advising the Secretary, the second largest federal agency, and the United States Congress on veteran’s issues as a veteran’s expert.",
       "Recently, Keith Streicher has served as the Interim Business Operations Manager for Rocketship Transformation Prep, Program Director for the Wisconsin Veterans Chamber of Commerce, and former Campaign Manager for Councilwoman Chantia Lewis-Chantia for Progress. Keith sits on the Rite Hite YMCA advisory board as Chair of the Annual Giving Campaign and Social Responsibility committee, Board of Trustees for Milwaukee’s Finest Scholars, and Board of Directors of Outpost Natural Foods, Milwaukee County War Museum VEI Committee, and the City of Milwaukee’s African American Male Initiative."
